b2ab32e0dfaafc4e7460c08da28b6b037e3041a5
jvscoelho28-source <jvscoelho28@gmail.com>
2025-08-12 20:34:17 -0300

    Create aula08_exercicio_01.py

---
+++ Aula08/aula08_exercicio_01.py
@@ -0,0 +1,28 @@
+"""
+Função Saudação
+   - Imprime uma mensagem de boas vindas
+"""
+def saudacao () :
+    print(f"n\n\tOlá usuário, seja bem vindo !!!\n\n")
+
+
+##
+# Corpo do Programa
+##
+#saudacao () 
+
+##
+# Somar dois numeros
+##
+
+
+def somar_numeros (n1, n2) :
+    soma = n1 + n2
+    return soma
+
+##
+# Corpo do Programa
+##
+#saudacao ()
+
+print(f"(somar_numeros (7, 25))")
