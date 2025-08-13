57502d568fa32c8a1799815a8370499e340b3d2f
jvscoelho28-source <jvscoelho28@gmail.com>
2025-08-12 21:49:33 -0300

    Create aula08_exercicio_02.py

---
+++ Aula08/aula08_exercicio_02.py
@@ -0,0 +1,34 @@
+def saudacao():
+    print("\n\n\tOlá, mundo!")
+
+if __name__ == "__main__":
+  #saudacao()
+
+  def subtrair_numeros_v4():
+   num1 = float(input("Digite o primeiro número: "))
+   num2 = float(input("Digite o segundo número: "))
+   num3 = float(input("Digite o terceiro número: "))
+   subtracao = num1 - num2 - num3
+   print(f"A subtracao é: {subtracao}")
+
+   if __name__ == "__subtracao__":
+      #subtracao()   
+
+def multiplicacao(a, b):
+   return a * b
+
+def divisao(a, b):
+   return a / b
+
+def main():
+   resultado_multiplicacao = multiplicacao(12, 5)
+   resultado_divisao = divisao(24, 3)
+   print(f"multiplicacao: {resultado_multiplicacao}")
+   print(f"divisao: {resultado_divisao}")
+
+   if __name__ == "__main__":
+      main()
+      
+
+
+
